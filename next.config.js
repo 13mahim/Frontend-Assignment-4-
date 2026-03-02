e0242f8cc7d9712a20b2a6ab866049b1ae64af51
Md Fahad Hasan <fahad13mahim@gmail.com>
2026-03-02 12:00:14 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+  images: {
+    domains: ['localhost'],
+  },
+}
 
 module.exports = nextConfig
